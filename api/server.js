d63c2a91032d7703e07e4f6a3ff6e8a212afef03
Emmanuel Padilla <manolin923@gmail.com>
2020-11-15 14:04:00 -0800

    foodtruck table setup and users model router working

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+require("colors");
+
+const server = express();
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(cors());
+server.use(express.json());
+
+// const userRouter = require('./users/userRouter')
+// server.use('/api/users', userRouter)
+
+server.get("/", (req, res) => {
+  res.send("the server is running");
+});
+
+module.exports = server;
