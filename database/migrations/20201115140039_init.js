8bf2991ced0ec8cfe743e3416aeac7fc5999c231
Emmanuel Padilla <manolin923@gmail.com>
2020-11-16 14:17:43 -0800

    migrations completed, seeds come next, prob mañana.

--- database/migrations/20201115140039_init.js
+++ database/migrations/20201115140039_init.js
@@ -1,26 +1,117 @@
 exports.up = function (knex) {
-    return knex.schema
-      .createTable("roles", tbl => {
-        tbl.increments();
-  
-        tbl.string("name", 128).notNullable().unique();
-      })
-      .createTable("users", tbl => {
-        tbl.increments();
-  
-        tbl.string("username", 128).notNullable().unique().index();
-        tbl.string("password", 256).notNullable();
-  
-        tbl
-          .integer("role")
-          .unsigned()
-          .references("roles.id")
-          .onDelete("RESTRICT")
-          .onUpdate("CASCADE");
-      });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTableIfExists("roles").dropTableIfExists("users");
-  };
-  
+  return knex.schema
+    .createTable("roles", (tbl) => {
+      tbl.increments("id");
+
+      tbl.string("name", 128).notNullable().unique();
+    })
+    .createTable("users", (tbl) => {
+      tbl.increments("id");
+
+      tbl.string("username", 128).notNullable().unique().index();
+      tbl.string("password", 256).notNullable();
+
+      tbl
+        .integer("role")
+        .unsigned()
+        .references("roles.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+    .createTable("OwnedTrucks", (tbl) => {
+      tbl.increments("id");
+      tbl
+        .integer("UserId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("TruckId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("TruckInfo")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("Diner", (tbl) => {
+      tbl.increments("id");
+      tbl
+        .integer("UserId")
+        .unsigned()
+        .notNullable()
+        .references("users.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.string("Location").notNullable();
+    })
+    .createTable("FavTrucks", (tbl) => {
+      tbl.increments("id");
+      tbl
+        .integer("UserId")
+        .unsigned()
+        .notNullable()
+        .references("users.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("TruckId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("TruckInfo")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("TruckInfo", (tbl) => {
+      tbl.increments("id")
+      tbl.string("Name").notNullable().unique()
+      tbl.string("imageURL")
+      tbl.string("cuisineType").notNullable()
+      tbl.integer("CustomerRating")
+      tbl.integer("TruckOwner").notNullable().references("OwnedTrucks.UserId").onDelete("CASCADE").onUpdate("CASCADE");
+      tbl.integer("CurrentLocation").notNullable().unsigned().references("Locations.id").onDelete("CASCADE").onUpdate("CASCADE");
+    })
+    .createTable("Locations", (tbl) => {
+      tbl.increments("id");
+      tbl.string("Location");
+      tbl.string("arrivalTime");
+      tbl.string("departureTime");
+    })
+    .createTable('TruckRatings', (tbl) =>{
+      tbl.increments("id")
+      tbl.integer('rating').unsigned()
+      tbl.integer('TruckId').unsigned().references('TruckInfo.id').onDelete("RESTRICT").onUpdate("RESTRICT");
+    })
+    .createTable('Menu', tbl =>{
+      tbl.increments('id')
+      tbl.string('Name').notNullable()
+      tbl.string('Description').notNullable()
+      tbl.string('photoURL')
+      tbl.integer('price').notNullable()
+      tbl.integer('rating')
+    })
+    .createTable('ItemRatings', (tbl) =>{
+      tbl.increments("id")
+      tbl.integer('rating').unsigned()
+      tbl.integer('TruckId').unsigned().references('TruckInfo.id').onDelete("RESTRICT").onUpdate("RESTRICT");
+      tbl.integer('ItemId').unsigned().references('Menu.id').onDelete("RESTRICT").onUpdate("RESTRICT");    
+    })
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("ItemRatings")
+    .dropTableIfExists("Menu")
+    .dropTableIfExists("TruckRatings")
+    .dropTableIfExists("Locations")
+    .dropTableIfExists("TrucksInfo")
+    .dropTableIfExists("FavTrucks")
+    .dropTableIfExists("Diner")
+    .dropTableIfExists("OwnedTrucks")
+    .dropTableIfExists("roles")
+    .dropTableIfExists("users");
+};
